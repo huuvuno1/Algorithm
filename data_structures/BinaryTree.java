9d35cf1bc2eb1fd01fb71a3ee2679534571307d0
Nguyễn Hữu Vũ <nguyenhuuvuno1@gmail.com>
2021-07-28 16:45:05 +0700

    add binary tree

---
+++ data_structures/BinaryTree.java
@@ -0,0 +1,5 @@
+package data_structures;
+
+public class BinaryTree<T> {
+    
+}
--- sorts/Main.java
+++ sorts/Main.java
@@ -7,28 +7,35 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(-110);
-        list.add(70);
-        list.add(14);
-        list.add(7);
-        list.add(-14);
-        list.add(3);
-        list.add(-41);
-        list.add(-10);
         list.add(-100);
+        list.add(-41);
         list.add(-15);
+        list.add(-14);
+        list.add(-10);
+        list.add(3);
+        list.add(7);
+        list.add(14);
         list.add(15);
+        list.add(70);
 
+        long t1 = System.currentTimeMillis();
         SelectionSort.sort(list, (o1, o2) -> o1 - o2);
+        System.out.println("select: " + (System.currentTimeMillis() - t1));
         print("Selection Sort (min -> max)", list);
 
+        t1 = System.currentTimeMillis();
         InsertionSort.sort(list, (o1, o2) -> o2 - o1);
+        System.out.println("Insertion: " + (System.currentTimeMillis() - t1));
         print("Insertion Sort (max -> min)", list);
 
+        t1 = System.currentTimeMillis();
         BubbleSort.sort(list, (o1, o2) -> o1 - o2);
+        System.out.println("Buble: " + (System.currentTimeMillis() - t1));
         print("Buble Sort (min -> max)", list);
 
-
+        t1 = System.currentTimeMillis();
         MergeSort.sort(list, (o1, o2) -> o2 - o1);
+        System.out.println("Merge: " + (System.currentTimeMillis() - t1));
         print("Merge Sort (max -> min)", list);
     }
 
